1fe933cb909a56cbc278fee7c99c08242c4b8708
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-11 10:28:17 +0200

    Create MVC project for tableView

---
+++ MVVM-2/MVVM-2/Model/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Michail Bondarenko on 2/11/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+    
+}
---
+++ MVVM-2/MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Michail Bondarenko on 2/11/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
